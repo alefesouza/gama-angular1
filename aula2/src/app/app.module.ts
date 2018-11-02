49fbad60f8cd1042013672d9b8db623ccb134088
Alefe Souza <contact@alefesouza.com>
2018-11-01 21:28:57 -0300

    Adiciona serviços e rotas

--- aula2/src/app/app.module.ts
+++ aula2/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+const appRoutes = [{
+  path: '', component: HomeComponent,
+}, {
+  path: 'todos', component: TodoListComponent,
+}];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
